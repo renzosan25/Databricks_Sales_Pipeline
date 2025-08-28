4a21ee8e1caf832a7f03ebb34b6715ebeeb05b37
renzosan25 <renzosanchez201@gmail.com>
2025-08-28 13:11:14 -0500

    s3-fix

--- SQL_Querys/table_sales_g.sql
+++ SQL_Querys/table_sales_g.sql
@@ -1,4 +1,7 @@
-CREATE OR REPLACE TABLE medallion.gold_sales_day AS
+CREATE OR REPLACE TABLE medallion.gold_sales_day
+USING DELTA
+LOCATION 's3://databricks-jw4u7ol1kfbswyqffnczbx-cloud-storage-bucket/gold/gold_sales_day'
+AS
 SELECT
     date,
     SUM(sales) As total_sales,
@@ -6,7 +9,10 @@ SELECT
 FROM medallion.sales_silver
 GROUP BY date;
 
-CREATE OR REPLACE TABLE medallion.gold_sales_store AS
+CREATE OR REPLACE TABLE medallion.gold_sales_store
+USING DELTA
+LOCATION 's3://databricks-jw4u7ol1kfbswyqffnczbx-cloud-storage-bucket/gold/gold_sales_store'
+AS
 SELECT
     store_id,
     SUM(sales) AS total_sales,
@@ -14,12 +20,13 @@ SELECT
 FROM medallion.sales_silver
 GROUP BY store_id;
 
-CREATE OR REPLACE TABLE medallion.gold_sales_product AS
+CREATE OR REPLACE TABLE medallion.gold_sales_product
+USING DELTA
+LOCATION 's3://databricks-jw4u7ol1kfbswyqffnczbx-cloud-storage-bucket/gold/gold_sales_product'
+AS
 SELECT
     product_id,
     SUM(sales) as Total_sales,
     SUM(revenue) AS total_revenue
 FROM medallion.sales_silver
 GROUP BY product_id;
-
-
